db0e7b09fa0115f2cd080ff1f7027670720d217b
abdul al noman <nomanmd1st2001@gmail.com>
2022-10-25 20:12:09 +0600

    added coursesdata

--- src/component/Pages/Courses/Courses.js
+++ src/component/Pages/Courses/Courses.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import Coursesdata from './Coursesdata/Coursesdata';
 
 const Courses = () => {
     const datas = useLoaderData()
     console.log(datas)
     return (
         <div>
-            <div className='flex justify-between h-screen container mx-auto'>
-                <div className='border w-1/4'>
+            <div className='flex justify-between container mx-auto'>
+                <div className='border w-1/4 p-4'>
                     {
-                        datas.map(data=><li className='text-decoration-none'><Link>{data.title}</Link></li>)
+                        datas.map(data=><li key={data.index} className='list-none mb-4'><Link>{data.title}</Link></li>)
                     }
                 </div>
-                <div className='border w-3/4'>world</div>
+                <div className='border w-3/4'>
+                    {
+                        datas.map(data=><Coursesdata key={data.index} data={data}></Coursesdata>)
+                    }
+                </div>
             </div>
         </div>
     );
---
+++ src/component/Pages/Courses/Coursesdata/Coursesdata.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Coursesdata = ({data}) => {
+    console.log(data)
+    return (
+        <div>
+            coursesdata
+        </div>
+    );
+};
+
+export default Coursesdata;
