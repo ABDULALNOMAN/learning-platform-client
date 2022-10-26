7eb7f731b6b7c906ba005dcecc2bd40139ecd20c
abdul al noman <nomanmd1st2001@gmail.com>
2022-10-26 10:05:35 +0600

    register add

--- src/component/Pages/Register/Register.js
+++ src/component/Pages/Register/Register.js
@@ -3,7 +3,44 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            register
+            <div className=" max-w-full min-h-screen bg-base-200 pt-3 capitalize">
+                <div className="hero-content flex-col">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold">Register now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 max-w-sm shadow-2xl w-11/12">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="enter your name" className="input input-bordered"/>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">image</span>
+                                </label>
+                                <input type="text" placeholder="image" className="input input-bordered"/>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered"/>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
