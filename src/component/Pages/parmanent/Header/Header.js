0a98153fdd7b957220421d833dc5f7e4df4e1721
abdul al noman <nomanmd1st2001@gmail.com>
2022-10-26 09:24:36 +0600

    toggle add

--- src/component/Pages/parmanent/Header/Header.js
+++ src/component/Pages/parmanent/Header/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
         <div className='bg-cyan-900 text-white capitalize'>
             <div className="navbar flex justify-between container mx-auto">
                 <div>
-                    <img className='w-12 h-12 rounded-full ' src={logo} alt="" />
-                    <Link to={'/'} className="btn btn-ghost text-xl uppercase italic ">Titan</Link>
+                    <img className='w-14 h-14 rounded-full ' src={logo} alt="" />
+                    <Link to={'/'} className=" btn-ghost text-xl uppercase italic ">Titan</Link>
                 </div>
                 <div>
                     <div className="navbar-end mr-4">
@@ -17,19 +17,21 @@ const Header = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </label>
                             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><Link to={'/'}>Home</Link></li>
                                 <li><Link to={'/courses'}>courses</Link></li>
-                                <li><Link to={'/login'}>Login</Link></li>
-                                <li><Link to={'/register'}>Register</Link></li>
+                                <li><Link to={''}>FAQ</Link></li>
+                                <li><Link to={''}>blog</Link></li>
+                                <input type="checkbox" className="toggle toggle-sm ml-4" checked />
+                                <li><Link to={'/login'}>login</Link></li>
                             </ul>
                         </div>
                     </div>
                     <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal p-0">
-                        <li><Link to={'/'}>Home</Link></li>
-                        <li><Link to={'/courses'}>courses</Link></li>
-                        <li><Link to={'/login'}>Login</Link></li>
-                        <li><Link className='/register'>register</Link></li>
+                        <ul className="menu menu-horizontal p-0 lg:flex items-center">
+                            <li><Link to={'/courses'}>courses</Link></li>
+                            <li><Link to={''}>FAQ</Link></li>
+                            <li><Link to={''}>Blog</Link></li>
+                            <input type="checkbox" className="toggle" checked />
+                            <li className='btn btn-warning rounded-md ml-2'><Link to={'/login'}>Login</Link></li>
                         </ul>
                     </div>
                 </div>
